a109b928beb538dee9b91edc20d53cf09ef726dd
guesswhozzz <torridtime@gmail.com>
2020-10-15 11:37:18 +0300

    proto

--- src/app/components/result/result.js
+++ src/app/components/result/result.js
@@ -13,48 +13,17 @@ const Result = (state) => {
     code: { codeline, bgVar, fgVar },
     getResultCodeLine,
   } = state;
+  
   useEffect(() => {
     getResultCodeLine();
   }, [resPsOneLine]);
+  
   return (
     <CodeResultWrapper>
       <CodeHeader />
       <CodeArea {...state} />
     </CodeResultWrapper>
   );
-  //   <div className="result--container">
-  //     <div className="result--limiter">
-  //       <div className="result-header">
-  //         <div className="result-header__title">
-  //           copy and paste into your .bashrc file
-  //         </div>
-  //         <button className="result-header__copybtn">Copy</button>
-  //       </div>
-  //       <table className="result">
-  //         <tbody>
-  //           <tr className="result__string">
-  //             <td className="result__line">#</td>
-  //             <td className="result__text"></td>
-  //           </tr>
-  //           <tr className="result__string">
-  //             <td className="result__line">#</td>
-  //             <td className="result__text"></td>
-  //           </tr>
-  //           <tr className="result__string">
-  //             <td className="result__line">#</td>
-  //             <td className="result__text"></td>
-  //           </tr>
-  //           <tr className="result__string">
-  //             <td className="result__line">#</td>
-  //             <td className="result__text">
-  //               {'export PS1="'}
-  //               {'"'}
-  //             </td>
-  //           </tr>
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   </div>
 };
 
 const mstp = (state) => {
