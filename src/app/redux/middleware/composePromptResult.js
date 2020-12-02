6923405d44c663bdd97e330bd93dcd537e94f1fc
guesswhozzz <torridtime@gmail.com>
2020-12-02 20:41:45 +0300

    Refactor composeprompt

--- src/app/redux/middleware/composePromptResult.js
+++ src/app/redux/middleware/composePromptResult.js
@@ -1,34 +1,25 @@
-//import update from "immutability-helper";
-const state = require("./teststate");
-
-///// Style varibale string  VAR=\[\e[DECORC1C2m\]
-///// style Sting 1 \[\e[ DECOR C1 C2 m\]
-
-//// prompt string PS1="${VAR}\s${RST}"
-
+const state = require('./teststate');
 const RST = {
   clipBorardString: 'RST="\\[\\e[0m\\]"\t#test\n',
 };
-
 const GIT_BRANCH = {
-  str1: "parse_git_branch() {",
+  str1: 'parse_git_branch() {',
   srt2: `    git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/\\1/'`,
-  srt3: "}",
+  srt3: '}',
 };
-
 const getDecorationCode = (code) => {
-  if (!code) return "";
+  if (!code) return '';
   return `${code};`;
 };
-const getColorCode = (pattern, code, bracket = "") => {
-  if (code === "RST") return "";
+const getColorCode = (pattern, code, bracket = '') => {
+  if (code === 'RST') return '';
   return `${pattern}${code}${bracket}`;
 };
 const createStyleString = (bg, fg) => {
-  const colorscode = "".concat(
-    getDecorationCode(""),
-    getColorCode("48;05;", bg, ";"),
-    getColorCode("38;05;", fg, "m")
+  const colorscode = ''.concat(
+    getDecorationCode(''),
+    getColorCode('48;05;', bg, ';'),
+    getColorCode('38;05;', fg, 'm')
   );
   return colorscode;
 };
@@ -36,24 +27,24 @@ const createStyleString = (bg, fg) => {
 const createStyleVariable = (i, lni, decor, colors) => {
   if (!colors) {
     return {
-      varString: "",
-      varName: "",
+      varString: '',
+      varName: '',
       syntax: [],
     };
   }
-  const esc = "\\e";
+  const esc = '\\e';
   const varName = `ST${i}${lni}`;
   return {
     varString: `${varName}="\\[${esc}[${decor}${colors}\\]"`,
     varName: `\$\{${varName}\}`,
     varSyntax: [
-      { text: varName, type: "shVariable" },
-      { text: `="\\[`, type: "shString" },
-      { text: esc, type: "shSpecial" },
-      { text: `[`, type: "shString" },
-      { text: decor, type: "shString" },
-      { text: colors, type: "shString" },
-      { text: "\\]", type: "shString" },
+      { text: varName, type: 'shVariable' },
+      { text: `="\\[`, type: 'shString' },
+      { text: esc, type: 'shSpecial' },
+      { text: `[`, type: 'shString' },
+      { text: decor, type: 'shString' },
+      { text: colors, type: 'shString' },
+      { text: '\\]', type: 'shString' },
     ],
   };
 };
@@ -63,7 +54,7 @@ const createPsOneSting = (
   fg,
   decor,
   type,
-  seqcode = "test",
+  seqcode = 'test',
   elIndex,
   lineIndex
 ) => {
@@ -75,7 +66,7 @@ const createPsOneSting = (
     decorString,
     styleString
   );
-  const RST = styleString ? `\$\{RST\}` : "";
+  const RST = styleString ? `\$\{RST\}` : '';
   return {
     varibale: {
       varString: varString,
@@ -84,13 +75,13 @@ const createPsOneSting = (
     psone: {
       psonePart: `${varName}${seqcode}${RST}`,
       psoneSyntax: [
-        { text: "${", type: "shPreProc" },
-        { text: `ST${elIndex}${lineIndex}`, type: "shVariable" },
-        { text: "}", type: "shPreProc" },
-        { text: `${seqcode}`, type: "shSpecial" },
-        { text: "${", type: "shPreProc" },
-        { text: "RST", type: "shVariable" },
-        { text: "}", type: "shPreProc" },
+        { text: '${', type: 'shPreProc' },
+        { text: `ST${elIndex}${lineIndex}`, type: 'shVariable' },
+        { text: '}', type: 'shPreProc' },
+        { text: `${seqcode}`, type: 'shSpecial' },
+        { text: '${', type: 'shPreProc' },
+        { text: 'RST', type: 'shVariable' },
+        { text: '}', type: 'shPreProc' },
       ],
     },
   };
@@ -103,7 +94,7 @@ const cmp2 = (arr) => {
       return createPsOneSting(
         elem.bg.colorId,
         elem.fg.colorId,
-        "",
+        '',
         elem.type,
         elem.code,
         elIndex,
@@ -117,9 +108,9 @@ const cmp2 = (arr) => {
 
 const cbres = (res) => {
   const psoneresstr = res.map((e, i, arr) => {
-    let start = i === 0 ? 'PS1="' : "";
-    let end = arr.length - 1 === i ? '"' : "";
-    let line = e.map((e) => e.psone.psonePart).join("");
+    let start = i === 0 ? 'PS1="' : '';
+    let end = arr.length - 1 === i ? '"' : '';
+    let line = e.map((e) => e.psone.psonePart).join('');
     return `${start}${line}${end}\n`;
   });
   const varibaleStr = res
@@ -129,148 +120,126 @@ const cbres = (res) => {
       return `${varstr}\n`;
     })
     .filter((e) => e.length > 1);
-  return [RST.clipBorardString, ...varibaleStr, "\n", ...psoneresstr];
+  return [RST.clipBorardString, ...varibaleStr, '\n', ...psoneresstr];
 };
 
-export const cmp = (arr) => {
-  const subres = cmp2(arr);
-  const pscbline = cbres(subres);
-  return {
-    res: subres,
-    pscbline: pscbline,
-  };
-};
-
-// let colorObject = {
-//   id: "",
-//   colorCode: "",
-//   colorName: "",
-// };
-// const LEFTBRACKET = { text: "${", type: "SH/BRACKET" };
-// const RIGHTBRACKET = { text: `}`, type: "SH/BRACKET" };
-// const STRING = { text: "", type: "SH/STRING" };
-// const VARIABLE = { text: "", type: "SH/VARIABLE" };
-
-// const setText = (group, _text) => {
-//   return update(group, { text: { $set: _text } });
-// };
-
-// const createUniqColorsMap = (arr) => {
-//   const { uniqColors } = arr.reduce(
-//     (acc, el) => {
-//       if (acc.map[el["colorId"]]) return acc;
-//       acc.map[el["colorId"]] = true;
-//       acc.uniqColors.push(el);
-//       return acc;
-//     },
-//     {
-//       uniqColors: [],
-//       map: {},
-//     }
-//   );
-//   return uniqColors;
-// };
-
-// const objectifyColors = (e, i, pattern, varname) => {
-//   if (e.colorId === "RST") {
-//     return colorObject;
-//   }
-//   return update(colorObject, {
-//     id: { $set: e.colorId },
-//     colorCode: { $set: `${pattern}${e.colorId}` },
-//     colorName: { $set: e.name },
-//   });
-// };
-
-// const getColors = (state) => {
-//   const uniqBgMap = createUniqColorsMap(state.flat().map((e) => e.bg)).map(
-//     (e, i) => {
-//       return objectifyColors(e, i, "48;05;", "BC");
-//     }
-//   );
-//   const uniqFgMap = createUniqColorsMap(state.flat().map((e) => e.fg)).map(
-//     (e, i) => {
-//       return objectifyColors(e, i, "38;05;", "FC");
-//     }
-//   );
+// export const cmp = (arr) => {
+//   const subres = cmp2(arr);
+//   const pscbline = cbres(subres);
 //   return {
-//     uniqBgMap: uniqBgMap,
-//     uniqFgMap: uniqFgMap,
+//     res: subres,
+//     pscbline: pscbline,
 //   };
 // };
 
-// // FIND AND CREATE UNIQ COLORS OBJ
-// const varName = (obj) => {
-//   if (!obj) {
-//     return [];
-//   }
-//   return [
-//     { text: "${", type: BRACKET },
-//     { text: `${obj.varName}`, type: VARIABLE },
-//     { text: `}`, type: BRACKET },
-//   ];
-// };
+const testStateSimple = {
+  id: 1,
+  text: 'The hostname (short)',
+  sequences: 'LocalHost',
+  code: '\\h',
+  style: ['regular'],
+  bg: {
+    colorInfo: false,
+    colorId: 'RST',
+    hexString: '#000000',
+    rgb: { r: 0, g: 0, b: 0 },
+    hsl: { h: 0, s: 0, l: 0 },
+    name: 'Black',
+  },
+  fg: {
+    colorId: 212,
+    hexString: '#ff87d7',
+    rgb: { r: 255, g: 135, b: 215 },
+    hsl: { h: 320, s: 100, l: 76 },
+    name: 'Orchid2',
+  },
+  type: 'SEQUENCES',
+};
 
-// const createPromptString = (statepart, bgmap, fgmap) => {
-//   let rcStringPart = statepart.map((curEl) => {
-//     const fgvar = fgmap.find((e) => curEl.fg.colorId === e.id);
-//     const bgvar = bgmap.find((e) => curEl.bg.colorId === e.id);
-//     const var1 = varName(fgvar);
-//     const var2 = varName(bgvar);
-//     const var3 = var1 || var2 ? "JN" : "есть";
-//     //const string = `\${${fgvar.varName}}\${${bgvar.varName}}${curEl.code}\$\{RST\}`;
-//     return {
-//       //  string: string,
-//       syntax: [
-//         ...var1,
-//         ...var2,
-//         { text: `${curEl.code}`, type: VARIABLE },
-//         ...var3,
-//         // { text: "${", type: BRACKET },
-//         // { text: "RST", type: VARIABLE },
-//         // { text: "}", type: BRACKET },
-//       ],
-//     };
-//   });
-//   return { rcStringPart: rcStringPart };
-// };
-// const rccode = (state, uniqBgMap, uniqFgMap) => {
-//   let a = state.map((e) => {
-//     return createPromptString(e, uniqBgMap, uniqFgMap);
-//   });
-//   return a;
-// };
+// PromptPart
+class Document {}
 
-// // CREATE PS STRING PARTS
-// const createVariableStrings = (colorarr) => {
-//   return colorarr.map((e) => {
-//     if (!e.varName) {
-//       return {
-//         string: ``,
-//         syntax: [],
-//       };
-//     }
-//     return {
-//       string: `${e.varName}="\\[\\e[${e.colorCode}m\\]"       #${e.colorName}`,
-//       syntax: [
-//         { text: e.varName, type: VARIABLE },
-//         { text: `"="\\[\\e[${e.colorCode}m\\]"`, type: STRING },
-//         { text: `#${e.colorName}`, type: COMMENT },
-//       ],
-//     };
-//   });
-// };
+class Product {
+  constructor() {
+    this.var = [];
+    this.part = [];
+    this.resetCode = '\\[\\e[0m\\]';
+    this.escCode = '\\e[';
+  }
+}
 
-// export const cmp = (state) => {
-//   const { uniqBgMap, uniqFgMap } = getColors(state);
-//   const rcFileStringParts = rccode(state, uniqBgMap, uniqFgMap);
-//   const fg = createVariableStrings(uniqFgMap);
-//   const bg = createVariableStrings(uniqBgMap);
-//   return {
-//     ps: rcFileStringParts,
-//     fg: fg,
-//     bg: bg,
-//   };
-// };
+class BashPropmpBuilder {
+  constructor(prop) {
+    this.prop = prop;
+  }
+  getVariableName(namePart) {
+    return `${namePart}${this.prop.id}`;
+  }
+}
 
-//console.log(cmp(state));
+class /** @specific */ VarBuilder extends BashPropmpBuilder {
+  constructor(prop) {
+    super(prop);
+    this.reset();
+  }
+  reset() {
+    this._product = new Product();
+  }
+  setVarName() {
+    const name = super.getVariableName('VR');
+    this._product.var.push(`${name}=`);
+  }
+  setFgColor() {
+    this._product.part.push(this.prop.fg.colorId);
+  }
+  setBgColor() {
+    this._product.part.push(this.prop.bg.colorId);
+  }
+  getPromptPart() {
+    return this._product.var;
+  }
+}
+
+class /** @specific */ Builder extends BashPropmpBuilder {
+  constructor(prop) {
+    super(prop);
+    this.reset();
+  }
+  reset() {
+    this._product = new Product();
+  }
+  setDecoration() {
+    const varName = super.getVariableName('VR');
+    this._product.part.push(`\${${varName}}`);
+  }
+  setSequences() {
+    this._product.part.push(this.prop.code);
+  }
+  getPromptPart() {
+    return this._product.part;
+  }
+}
+class Director {
+  constructor(builder) {
+    this._builder = builder;
+  }
+  createPsoneString() {
+    this._builder.setDecoration();
+    this._builder.setSequences();
+  }
+  createVariableString() {
+    this._builder.setVarName();
+  }
+}
+
+function client(prop) {
+  const builder = new Builder(prop);
+  const director = new Director(builder);
+  const varbuilder = new VarBuilder(prop);
+  const vardirector = new Director(varbuilder);
+  director.createPsoneString();
+  vardirector.createVariableString();
+  return { 1: builder.getPromptPart(), 2: varbuilder.getPromptPart() };
+}
+
+console.log(client(testStateSimple));
